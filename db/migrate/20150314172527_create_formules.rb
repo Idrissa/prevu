ab516a0f501dd6d71de9539fab3c7e0fa372a1df
sidya <scicasoft@gmail.com>
2015-03-14 17:59:44 +0000

    gestion des structures assurances et formules

--- db/migrate/20150314172527_create_formules.rb
+++ db/migrate/20150314172527_create_formules.rb
@@ -2,11 +2,11 @@ class CreateFormules < ActiveRecord::Migration
   def change
     create_table :formules do |t|
       t.references :structure_assurance, index: true
-      t.string :nom
-      t.integer :periode
-      t.integer :occurrence_periode, default: 1
-      t.float :montant_adhesion
-      t.float :montant_cotisation
+      t.string :nom, null: false
+      t.integer :periode, null: false
+      t.integer :occurrence_periode, default: 1, null: false
+      t.float :montant_adhesion, null: false
+      t.float :montant_cotisation, null: false
 
       t.timestamps null: false
     end
